accd411ad1e0d5b693c65bd36377205ec4e357dd
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-01-02 13:24:54 +0900

    useStateでswitchを操作できるようにした

--- pages/labo.tsx
+++ pages/labo.tsx
@@ -1,11 +1,20 @@
 import Layout from '../components/layout'
 import Switch from '../components/Switch'
+import { useState } from 'react';
 
 export default function FirstPost() {
+  const [bool, setBool] = useState(false);
+
+  const clickFunc = () => {
+    setBool(!bool)
+  }
+
   return (
     <Layout>
       <h1>Labo</h1>
-      <Switch />
+      <Switch bool={bool} checkedFunc={setBool} />
+      <button onClick={clickFunc}>test</button>
+      { bool === true ? <p>on</p> : <p>off</p> }
     </Layout>
   )
 }
